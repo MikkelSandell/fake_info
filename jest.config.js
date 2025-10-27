2ad2b15a1d07827aedcf8a294ebd273c5019e953
Nicolai Bo Kunkel <nicolai@kunkel.dk>
2025-10-27 11:41:27 +0100

    fix for ci

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   collectCoverageFrom: ['src/**/*.js'],
-  coverageReporters: ['text', 'lcov', 'html']
+  coverageReporters: ['text', 'lcov', 'html'],
+
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/tests/integration/db_direct_query.test.js',
+  ],
 };
